b2fe55a997b9c50e62f7f2f049238d0b94f78af5
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-04-30 09:42:06 +0200

    Style layout

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -2,15 +2,20 @@ import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import "./normalize.css"
+import "../scss/components/layout.scss"
 
 export default ({ children }) => (
   <div
-    style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      minHeight: "100vh",
+    }}
     className="site-wrapper"
   >
     <Header />
     <main>
-      <div className="main-container">{children}</div>
+      <div className="container">{children}</div>
     </main>
     <div style={{ flex: "1" }}></div>
     <Footer />
