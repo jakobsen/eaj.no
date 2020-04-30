cb5dea217d80b5fa55bbdb4bbc88e74a74d36143
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-04-30 09:33:24 +0200

    Modify footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,46 @@
 import React from "react"
-import Github from "../images/github.svg"
+import Octocat from "../images/Octocat.png"
+import Netlify from "../images/netlify.svg"
+import Trondheim from "../images/Trondheim.svg"
+import Gatsby from "../images/Gatsby-Monogram.svg"
 import "../scss/components/footer.scss"
 
 export default () => {
   return (
-    <footer>
-      <div className="footer-container">
-        <a href="https://github.com/jakobsen">
-          <Github />
+    <footer className="footer container">
+      <div>Contact me.</div>
+      <div className="footer-links">
+        <a
+          href="https://github.com/jakobsen"
+          title="See the source on GitHub"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Octocat} alt="GitHub" />
+        </a>
+        <a
+          href="https://netlify.com"
+          title="Hosted with Netlify"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Netlify} alt="Netlify" />
+        </a>
+        <a
+          href="https://gatsbyjs.org"
+          title="Built with Gatsby"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Gatsby} alt="Gatsby" />
+        </a>
+        <a
+          href="https://en.wikipedia.org/wiki/Trondheim"
+          title="Made in Trondheim "
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Trondheim} alt="Trondheim" />
         </a>
       </div>
     </footer>
