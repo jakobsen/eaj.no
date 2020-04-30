d8954599f9863e13c50413e42a23f3d05bd456f8
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-04-30 11:23:56 +0200

    Remove breaking import

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
-import "./normalize.css"
 import "../scss/components/layout.scss"
 
 export default ({ children }) => (
