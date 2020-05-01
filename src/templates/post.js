abf1ea7c2754761b2f1241750f84cd3a972b8f44
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-05-01 15:20:05 +0200

    Add link to edit articles on GitHub

--- src/templates/post.js
+++ src/templates/post.js
@@ -8,7 +8,9 @@ require(`katex/dist/katex.min.css`)
 
 class PostTemplate extends React.Component {
   render() {
+    const regex = /eaj-no(\/.*)/gm
     const post = this.props.data.markdownRemark
+    const relativePath = regex.exec(post.fileAbsolutePath)[1]
     const siteTitle = this.props.data.site.siteMetadata.title
     const image = post.frontmatter.thumbnail
       ? post.frontmatter.thumbnail.childImageSharp.rezise
@@ -26,6 +28,16 @@ class PostTemplate extends React.Component {
             <div className="frontmatter">
               <h1>{post.frontmatter.title}</h1>
               <h4 className="date">{post.frontmatter.date}</h4>
+              <h4>
+                <a
+                  href={`https://github.com/jakobsen/eaj.no/blob/master${relativePath}`}
+                >
+                  <span role="img" aria-label="pencil">
+                    ✏️
+                  </span>
+                  Edit this article on GitHub
+                </a>
+              </h4>
             </div>
             <Img fixed={post.frontmatter.thumbnail.childImageSharp.fixed} />
           </div>
@@ -48,8 +60,8 @@ export const query = graphql`
     }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
-      excerpt(pruneLength: 160)
       html
+      fileAbsolutePath
       frontmatter {
         title
         date(formatString: "MMM D, YYYY")
