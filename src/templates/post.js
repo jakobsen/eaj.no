220aed6bfde270bc93127d3f121d1d86363025bd
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-05-11 23:29:58 +0200

    Add link to Twitter after each article

--- src/templates/post.js
+++ src/templates/post.js
@@ -47,6 +47,17 @@ class PostTemplate extends React.Component {
           <MDXProvider>
             <MDXRenderer>{post.body}</MDXRenderer>
           </MDXProvider>
+          <p style={{ fontSize: "0.8rem", marginTop: "100px" }}>
+            Do you have feedback on this article? Let me know on{" "}
+            <a
+              href="twitter.com/eaj_no"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+            .
+          </p>
         </article>
       </Layout>
     );
